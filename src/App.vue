fe3a5872c0ad256912ecc07aca83dca36fd85fd2
zxfjd3g <258147149@qq.com>
2018-07-31 09:49:34 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    App
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    color red
+</style>
