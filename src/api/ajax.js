20f9338b0233732e9455a4478aa108de76df5046
zxfjd3g <258147149@qq.com>
2018-07-31 14:54:34 +0800

    ajax

---
+++ src/api/ajax.js
@@ -0,0 +1,49 @@
+/*
+发送ajax请求的函数模块
+函数的返回值是promise对象
+ */
+
+export default function ajax(url, data={}, type='GET') {
+
+  // 返回一个自己创建的promsie对象, 为了能得到的不是response, 而是response.data的数据
+  return new Promise(function (resolve, reject) {
+    let promsie
+    if(type==='GET') {// GET请求
+      // 将data中所有数据作为请求参数拼到url中
+      // /register?username=tom&password=123
+      let queryStr = ''
+      Object.keys(data).forEach(key => {  // keys()得到对象自身的所有属性名组成的数组
+        const value = data[key]
+        queryStr += key + '=' + value + '&'
+      })
+      if(queryStr) { // username=tom&password=123&-->username=tom&password=123
+        // 去除最后的&
+        queryStr = queryStr.substring(0,queryStr.length-1) // username=tom&password=123
+        queryStr = '?' + queryStr //?username=tom&password=123
+      }
+
+      promsie = axios.get(url + queryStr)
+    } else { // POST请求
+      promsie = axios.post(url, data)
+    }
+
+    promsie.then(reponse => {
+      // 请求成功
+      resolve(response.data)
+    }).catch(error => {
+      // 请求失败
+      reject(error)
+    })
+  })
+}
+/*
+const promise = ajax('/xxx')
+promise.then(response => {
+  const result = response.data
+
+})
+const response = await ajax('/xxx')
+const result = response.data
+
+const result = await ajax('/xxx')
+*/
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Msite from '../pages/Msite/Msite.vue'
 import Profile from '../pages/Profile/Profile.vue'
 import Search from '../pages/Search/Search.vue'
 import Order from '../pages/Order/Order.vue'
+import Login from '../pages/Login/Login.vue'
 
 // 声明使用插件
 Vue.use(VueRouter)
@@ -17,19 +18,35 @@ export default new VueRouter({ // 配置对象
   routes: [
     {
       path: '/msite',
-      component: Msite
+      component: Msite,
+      meta: {
+        showFoot: true
+      }
     },
     {
       path: '/profile',
-      component: Profile
+      component: Profile,
+      meta: {
+        showFoot: true
+      }
     },
     {
       path: '/search',
-      component: Search
+      component: Search,
+      meta: {
+        showFoot: true
+      }
     },
     {
       path: '/order',
-      component: Order
+      component: Order,
+      meta: {
+        showFoot: true
+      }
+    },
+    {
+      path: '/login',
+      component: Login
     },
     {
       path: '/',
